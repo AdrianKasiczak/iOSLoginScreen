eb1f5f8475804e9559c34e72cc1fb84a0d9611f8
Adrian Kasiczak <adrian.kasiczak@icloud.com>
2018-02-15 23:25:37 +0100

    created sign in button + logo label, TODO: Figure out something with color pattern

--- iOSLoginScreen/ViewController.swift
+++ iOSLoginScreen/ViewController.swift
@@ -13,12 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 112/255, alpha: 1)
+        view.backgroundColor = UIColor(red: 80/255, green: 220/255, blue: 100/255, alpha: 1)
+        
+        setupContraints()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    let emailTextField: UITextField = {
+       let tf = UITextField()
+        tf.placeholder = "Type email here"
+        tf.textColor = .black
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    func setupContraints() {
+        view.addSubview(emailTextField)
+        
+        emailTextField.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
+        emailTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        emailTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 
